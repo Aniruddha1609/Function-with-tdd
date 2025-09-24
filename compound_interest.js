de6213426f7486c0568892d9f9a15d8c6b19f035
Aniruddha1609 <117630168+Aniruddha1609@users.noreply.github.com>
2025-09-24 15:17:56 +0530

    Create compound_interest.js

---
+++ compound_interest.js
@@ -0,0 +1,34 @@
+function compoundInterest(principle, time, rateOFInterest, expectedValue)  {
+    let updatedPrincipal = principle;
+    
+    for(let currentYearNumber = 1; currentYearNumber <= time; currentYearNumber++ ) {
+    let rateOFInterestPerYear = (updatedPrincipal * rateOFInterest) / 100;
+    updatedPrincipal = (updatedPrincipal + rateOFInterestPerYear);
+  }
+  let totalCompoundInterest = updatedPrincipal - principle;
+  return totalCompoundInterest;
+}
+
+function printCompoundInterest(principle, time, rateOFInterest, expectedValue) {
+
+  let interest = compoundInterest(principle, time, rateOFInterest, expectedValue);
+  
+  if(interest === expectedValue) {
+    console.log('✅ expected interest is ' + expectedValue + ' and your result is '+  interest);
+  } else {
+    console.log('❌ expected interest is ' + expectedValue + ' and your result is '+  interest);
+  }
+}
+
+function printAllInterest() {
+ printCompoundInterest(0, 1, 2, 0);   
+ printCompoundInterest(100, 2, 10, 21);	
+ printCompoundInterest(1200, 3, 6.5, 249.53954999999996);
+ printCompoundInterest(543, 3, 12, 219.875904);
+}
+
+printAllInterest();
+
+
+
+
