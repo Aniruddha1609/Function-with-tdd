4edff40a80a7330fbdf67300e5ff563cb8fe515a
Aniruddha1609 <117630168+Aniruddha1609@users.noreply.github.com>
2025-09-24 14:57:18 +0530

    Create simple_interest.js

---
+++ simple_interest.js
@@ -0,0 +1,23 @@
+function simpleInterest(principle, time, rateOFInterest, expectedValue)  {
+  return (principle * time * rateOFInterest) / 100;
+}
+
+function printValue(principle, time, rateOFInterest, expectedValue) {
+  let simpleInterestvalue = simpleInterest(principle, time, rateOFInterest, expectedValue);
+  
+  if(simpleInterestvalue === expectedValue) {
+    console.log('✅ expected interest is ' + expectedValue + ' and your result is '+  simpleInterestvalue);
+  } else {
+    console.log('❌ expected interest is ' + expectedValue + 'and your result is '+  simpleInterestvalue);
+  }
+}
+
+function printAllInterest() {
+  printValue(100, 1, 10, 10);
+  printValue(100, 2, 10, 20);
+  printValue(500, 5, 10, 250);
+  printValue(1000, 1, 5, 50);
+  printValue(2000, 3, 20, 1200);
+}
+
+printAllInterest();
